043cd9ad5ac11380b7a65f54c6ce11dc5aee3f55
Chadi-k <chadialkerdi@gmail.com>
2024-12-19 13:12:03 +0100

    mail_v1

--- main.py
+++ main.py
@@ -29,13 +29,14 @@ def getCSVE(feed_list) :
         csve_list.append(str(url))
         response = requests.get(url)
         jsoned_response = response.json()
+
+        
         feed['cves'] = jsoned_response['cves']
 
         
 
 getCSVE(rss_feed_list)
 
-test pull 
 
 """
 for item in rss_feed_list : 
@@ -45,3 +46,15 @@ for item in rss_feed_list :
 """
 
 #ENRICHISSEMENT CVE 
+
+
+
+#Génération d'Alertes et Notifications Email
+
+from mail import send_email
+
+critical_vuln = True  
+if critical_vuln:
+    subject = "Alerte CVE critique détectée"
+    body = "Une vulnérabilité critique vient d'être détectée. Veuillez prendre les mesures nécessaires."
+    send_email("admin@exemple.com", subject, body)
